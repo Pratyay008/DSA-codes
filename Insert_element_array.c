e7efbb0e434a15cd0ce1ecf04be7167fe94a0d61
Pratyay Mondal <81563083+pratyay9@users.noreply.github.com>
2021-12-22 19:20:31 +0530

    Add files via upload

---
+++ Insert_element_array.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{  
+    printf("---------------Insert one element in array--------------\n");
+    int x, n, temp;
+    int arr[8]={1,2,3,4,5,6,7,8};
+    printf("Enter the position : ");
+    scanf("%d", &n);
+    if (n>=1 && n<=9)
+    {
+        printf("Enter the element : ");
+        scanf("%d", &x);
+            
+        
+        for (int i = n-1; i <= 8; i++)
+        {
+            temp = arr[i];
+            arr[i] = x;
+            x = temp;
+        }
+
+        printf("The new elements are :\n");
+        for (int i = 0; i < 9; i++)
+        {
+            printf("%d\n", arr[i]);
+            
+        }
+    }
+    else
+    {
+        printf("Wrong position\n");
+    }
+    
+       
+ 
+
+
+
+    
+    return 0;
+}
+
+
+
+
